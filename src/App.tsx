7dd4ccceaf2b43695179f09b3a01de94de845f47
Leroy Wan <leroywan@gmail.com>
2021-06-08 13:12:40 -0400

    Add customers interface

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,44 @@ import { useTranslation } from "react-i18next";
 import "./locales/i18n";
 import axios from "axios";
 
-interface Customers {
-  id: string;
+interface Customer {
+  id: number;
   name: string;
+  email: string;
+  channel: string;
+  address: string;
+  postal: string;
+  city: string;
+  province: string;
+  country: string;
+}
+
+interface GetCustomerResponse {
+  customers: Customer[];
 }
 
 function App() {
   const { t } = useTranslation();
-  const [customers, setCustomers] = React.useState<Customers[]>();
+  const [isLoading, setIsLoading] = React.useState<Boolean>(true);
+  const [customers, setCustomers] = React.useState<Customer[]>([]);
 
   React.useEffect(() => {
     const getCustomers = async () => {
-      const { data } = await axios.get(
+      const response = await axios.get<GetCustomerResponse>(
         "https://rawgit.com/wvchallenges/se-exp-challenge-invoice/master/settings.json"
       );
 
-      setCustomers(data.customers);
+      setCustomers(response.data.customers);
+      setIsLoading(false);
     };
     getCustomers();
   }, []);
 
-  if (!customers) {
-    return null;
-  }
+  const customersMarkup = !isLoading ? (
+    customers.map((customer) => <li key={customer.id}>{customer.name}</li>)
+  ) : (
+    <p>loading...</p>
+  );
 
   return (
     <div className={styles.App}>
@@ -35,9 +50,7 @@ function App() {
       </div>
       <div className={styles.Body}>
         <h1 className={styles.Heading}>{t("heading")}</h1>
-        {customers.map((customer) => (
-          <li key={customer.id}>{customer.name}</li>
-        ))}
+        {customersMarkup}
       </div>
     </div>
   );
